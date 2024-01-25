7665c1247a9a312ea75f37d38874a118ae9a3792
Justin Palmer <228780+layoutd@users.noreply.github.com>
2024-01-25 11:16:00 +0100

    Only valid web_storage types

--- dist/sourcebuster.js
+++ dist/sourcebuster.js
@@ -279,8 +279,13 @@ module.exports = Object.assign({}, default_cookies, {
 var storageModule = null;
 
 module.exports = {
+	validateType: function( storage_type ) {
+		// Default to valid_values[0] if storage_type is not in valid_values
+		var valid_values = ['cookies', 'singleCookie', 'localStorage', 'sessionStorage'];
+		return valid_values.indexOf( storage_type ) > -1 ? storage_type : valid_values[0];
+	},
 	set: function(module) {
-		storageModule = module;
+		storageModule = this.validateType( module );
 	},
 	get: function() {
 		return storageModule;
@@ -828,7 +833,8 @@ module.exports = {
     // Set `session length` in minutes
     params.session_length = this.validate.checkInt(user.session_length) || 30;
 
-    params.local_storage = true;
+    // Determine the `web_storage` to use
+    params.web_storage = 'cookies';
 
     // Set `timezone offset` in hours
     params.timezone_offset = this.validate.checkInt(user.timezone_offset);
--- src/js/helpers/storage_init.js
+++ src/js/helpers/storage_init.js
@@ -1,8 +1,13 @@
 var storageModule = null;
 
 module.exports = {
+	validateType: function( storage_type ) {
+		// Default to valid_values[0] if storage_type is not in valid_values
+		var valid_values = ['cookies', 'singleCookie', 'localStorage', 'sessionStorage'];
+		return valid_values.indexOf( storage_type ) > -1 ? storage_type : valid_values[0];
+	},
 	set: function(module) {
-		storageModule = module;
+		storageModule = this.validateType( module );
 	},
 	get: function() {
 		return storageModule;
