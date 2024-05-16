197e9c8b942306e32575fcb87ff07fd5bfb6c25b
Justin Palmer <228780+layoutd@users.noreply.github.com>
2024-05-16 11:28:47 +0200

    Updated dist and demo files

--- dist/sourcebuster.js
+++ dist/sourcebuster.js
@@ -192,14 +192,14 @@ module.exports = {
       while (c.charAt(0) === ' ') { c = c.substring(1, c.length); }
       if (c.indexOf(nameEQ) === 0) {
         var cookie_content = c.substring(nameEQ.length, c.length);
-        // Decode the content if it is only base64-valid characters
-        // Non-base64 characters indicate that the cookie is in the old format
+        // Decode the content if it is only Base64-valid characters
+        // Non-Base64 characters indicate that the cookie is in the old format
         if (/^[A-Za-z0-9+/]+$/.test(cookie_content)) {
           try {
             // Ensure the Base64 string has the correct padding (always multiples of 4)
-            cookie_content = cookie_content.padEnd(cookie_content.length + (4 - cookie_content.length % 4) % 4, '=');
+            var padded_cookie_content = cookie_content.padEnd(cookie_content.length + (4 - cookie_content.length % 4) % 4, '=');
             // Decode the potentially padded Base64 string
-            cookie_content = atob(cookie_content);
+            cookie_content = atob( padded_cookie_content );
           } catch (error) {
             // If the Base64 string is invalid, just keep the cookie content as is
           }
